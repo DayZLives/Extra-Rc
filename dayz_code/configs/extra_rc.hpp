052d604618e8941a8e3c86b664816a1be796616e
worldwidesorrow <jsnmerrill@yahoo.com>
2017-12-24 13:40:49 -0600

    Update extra_rc.hpp

--- dayz_code/configs/extra_rc.hpp
+++ dayz_code/configs/extra_rc.hpp
@@ -1,5 +1,18 @@
 class ExtraRc
 {
+	class Classname of Item/Weapon/Tool in inventory
+	{
+		class Name of the right click action (anything you want)
+		{
+			text = "Display name of action";
+			script = "path to script to execute or code snippet to execute.";
+        };
+    };
+};
+
+
+/* Example used in WAI, everything goes between the open and close curly brackets of class ExtraRc
+
 	class ItemRadio
 	{
 		class switchOnOff
@@ -8,4 +21,5 @@ class ExtraRc
 			script = "execVM 'scripts\radio\switch_on_off.sqf'";
         };
     };
-};
+
+*/
