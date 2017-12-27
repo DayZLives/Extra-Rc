55147bcbbee798b23ed0951d7f945697e9875bcd
worldwidesorrow <jsnmerrill@yahoo.com>
2017-12-27 06:06:28 -0600

    Update extra_rc.hpp

--- dayz_code/configs/extra_rc.hpp
+++ dayz_code/configs/extra_rc.hpp
@@ -1,24 +1,24 @@
 class ExtraRc
 {
+	class ItemRadio
+		{
+			class switchOnOff
+			{
+				text = "Power ON/OFF";
+				script = "execVM 'scripts\radio\switch_on_off.sqf'";
+        	};
+   	 };	
+};
+
+
+/* This is an example of how the entries should look.
+
 	class Classname of Item/Weapon/Tool in inventory
 	{
 		class Name of the right click action (anything you want)
 		{
 			text = "Display name of action";
 			script = "path to script to execute or code snippet to execute.";
-        };
-    };
-};
-
-
-/* Example used in WAI, everything goes between the open and close curly brackets of class ExtraRc
-
-	class ItemRadio
-	{
-		class switchOnOff
-		{
-			text = "Power ON/OFF";
-			script = "execVM 'scripts\radio\switch_on_off.sqf'";
         };
     };
 
